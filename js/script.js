1b382efd266f1000af2546064b8b09797f54e860
Michal Kotlinski <michalkotlinski@gmail.com>
2023-08-19 17:16:50 +0200

    przeniesienie nasłuchiwaczy do init

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
-{
 const init = () => {
   const bodyElement = document.querySelector(".js-body");
   const button = document.querySelector(".js-button");
   const picture = document.querySelector(".js-picture");
   const themeButtonElement = document.querySelector(".js-themeButton");
   const headerElement = document.querySelectorAll(".js-header");
+
+  button.addEventListener("click", () => {
+    picture.hidden = !picture.hidden;
+  });
+
+  themeButtonElement.addEventListener("click", () => {
+    bodyElement.classList.toggle("body--grayTheme");
+    bodyElement.classList.toggle("body--whiteTheme");
+
+    themeButtonElement.innerText = bodyElement.classList.contains(
+      "body--grayTheme"
+    )
+      ? "Mniej szarości"
+      : "Więcej szarości";
+
+    headerElement.forEach((header) => header.classList.toggle("grayTheme"));
+  });
 };
 
-button.addEventListener("click", () => {
-  picture.hidden = !picture.hidden;
-});
-
-themeButtonElement.addEventListener("click", () => {
-  bodyElement.classList.toggle("body--grayTheme");
-  bodyElement.classList.toggle("body--whiteTheme");
-
-  themeButtonElement.innerText = bodyElement.classList.contains(
-    "body--grayTheme"
-  )
-    ? "Mniej szarości"
-    : "Więcej szarości";
-
-  headerElement.forEach((header) => header.classList.toggle("grayTheme"));
-});
-
 init();
-}
-
